9bfe9d997d8b0f362ec6838da8603d68f0ff0646
kabinny <kabinny@gmail.com>
2023-02-24 21:32:36 +0900

    jwt 구현

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+const privateKey = "secret"
 
 app.use(express.json())
 app.use(cors())
@@ -16,7 +19,10 @@ app.post("/login", (req, res) => {
     // ID는 있음
     if (userInfo[userIndex].password === password) {
       // password 맞음
-      res.status(200).send("로그인 성공")
+      res.status(200).json({
+        msg: "로그인 성공",
+        accessToken: jwt.sign({ userId: id }, privateKey),
+      })
       return
     }
   }
