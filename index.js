ef8e5278a7df9c42bfa890e657ace512c73d525a
kabinny <kabinny@gmail.com>
2023-02-24 21:22:28 +0900

    회원가입, 로그인 기능 구현

--- index.js
+++ index.js
@@ -5,8 +5,43 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("API 요청 성공")
+const userInfo = []
+
+// 로그인
+app.post("/login", (req, res) => {
+  // body id, password
+  const { id, password } = req.body
+  const userIndex = userInfo.findIndex((item) => item.id === id)
+  if (userIndex > -1) {
+    // ID는 있음
+    if (userInfo[userIndex].password === password) {
+      // password 맞음
+      res.status(200).send("로그인 성공")
+      return
+    }
+  }
+
+  res.status(500).send("로그인 실패")
+})
+
+// 회원가입
+app.post("/signIn", (req, res) => {
+  console.log(req.body)
+  // body id, password
+  const { id, password } = req.body
+
+  if (userInfo.findIndex((item) => item.id === id) > -1) {
+    res.status(500).send("회원가입 실패")
+    return
+  }
+
+  userInfo.push({
+    id,
+    password,
+  })
+  console.log("userInfo", userInfo)
+
+  res.send("회원가입 성공")
 })
 
 const port = 3000
